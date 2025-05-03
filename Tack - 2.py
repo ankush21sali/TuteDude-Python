b81a4ec6e739b5dc81b753b72fc9713d47419422
Ankush Sali <saliankush21@gmail.com>
2025-05-03 16:15:46 +0530

    Add files via upload
    Added newly task in asssigment - 1

---
+++ Tack - 2.py
@@ -0,0 +1,6 @@
+f_name = input("Enter Your first name : ")
+l_name = input("Enter Your second name : ")
+
+full_name = (f_name + " ") + (l_name + "!")
+
+print("Hello,",full_name,"Welcome to the Python program.")
