001be1c326d73050057a9cd39e6a8e44cb253614
Ankush Sali <saliankush21@gmail.com>
2025-05-03 16:18:47 +0530

    Add files via upload

---
+++ Assigment - 1st/Task - 1.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter the First Number : "))
+num2 = int(input("Enter the Second Number : "))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print("Addition is : ", add)
+print("Subtraction is : ", sub)
+print("Multiplication : ", mul)
+print("Division : ", div)
+
